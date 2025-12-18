9b9927a9bbb2ea05229015138d238838d6f7bebf
neskech <neskechastro@gmail.com>
2025-12-18 16:45:09 -0500

    Bug fix

--- modules/desktop/themes/hyprland/plymouth.nix
+++ modules/desktop/themes/hyprland/plymouth.nix
@@ -7,9 +7,6 @@
     # Ensure we override the package to get the correct color variant
     themePackages = [ (pkgs.catppuccin-plymouth.override { variant = "macchiato"; }) ];
   };
-  
-  # Force the bootloader to use the splash screen
-  boot.loader.timeout = 0;
 
   boot.kernelParams = [
     "video=efifb:off"
